312b45af786b04d6bc6a57ec7d3e7e60455d2595
ThinkCat <624432528@qq.com>
2015-05-25 17:58:20 +0800

    服务端tcp粘包处理

--- src/main/java/com/wang/server/handler/ChildChannelHandler.java
+++ src/main/java/com/wang/server/handler/ChildChannelHandler.java
@@ -2,6 +2,8 @@ package com.wang.server.handler;
 
 import io.netty.channel.ChannelInitializer;
 import io.netty.channel.socket.SocketChannel;
+import io.netty.handler.codec.LineBasedFrameDecoder;
+import io.netty.handler.codec.string.StringDecoder;
 
 
 /*
@@ -10,6 +12,10 @@ import io.netty.channel.socket.SocketChannel;
 public class ChildChannelHandler extends ChannelInitializer<SocketChannel> {
         @Override
         protected void initChannel(SocketChannel socketChannel) throws Exception {
+                //add tcp decoder
+                socketChannel.pipeline().addLast(new LineBasedFrameDecoder(1024));
+                socketChannel.pipeline().addLast(new StringDecoder());
+                //add Server handler
                 socketChannel.pipeline().addLast(new TimeServerHandler());
         }
 }
